ea32e7cb38868537a400640ec2c818290e7f6c9d
Mrinal9999 <158474730+Mrinal9999@users.noreply.github.com>
2025-06-14 04:46:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -29,3 +29,7 @@ def upload():
     finally:
         if os.path.exists(temp_path):
             os.remove(temp_path)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
